c60e1e981e9e88e59e5175401fff34effe73b0ab
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-01-26 10:00:38 +0100

    add components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
